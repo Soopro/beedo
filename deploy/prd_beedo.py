0c69386e98e0600d91664b7933fdf2c4375a1dec
redy <redy.ru@gmail.com>
2017-05-03 02:42:08 +0800

    xxx

--- deploy/prd_beedo.py
+++ deploy/prd_beedo.py
@@ -7,3 +7,7 @@ workers = multiprocessing.cpu_count() * 2 + 1
 accesslog = 'logs/access.log'
 errorlog = 'logs/error.log'
 pidfile = 'logs/pid.pid'
+
+_log_str = '%(t)s %(u)s \'%(r)s\' %(s)s %(b)s \'%(f)s\''
+_log_ip = '<%({X-Real-IP}i)s> [%({X-Forwarded-for}i)s]'
+access_log_format = '{} {}'.format(_log_str, _log_ip)
