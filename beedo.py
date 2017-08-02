a91c360d3a9151f9438cefffa696a21728548705
redy <redy.ru@gmail.com>
2017-08-03 02:55:34 +0800

    add files watching

--- beedo.py
+++ beedo.py
@@ -7,12 +7,13 @@ from flask.json import JSONEncoder
 import os
 import traceback
 
-from loaders import load_config, load_files, load_keys
+from loaders import (load_config, load_files, load_keys,
+                     load_single_file, watch_files_date)
 from services.i18n import Translator
 from blueprints import register_blueprints
 
 
-__version_info__ = ('1', '1', '1')
+__version_info__ = ('1', '2', '0')
 __version__ = '.'.join(__version_info__)
 
 # create app
@@ -40,6 +41,7 @@ register_blueprints(app)
 app.db = dict()
 app.db['files'] = load_files(app)
 app.db['keys'] = load_keys(app, app.db['files'])
+app.db['modifications'] = watch_files_date(app)
 
 
 # languages
@@ -51,6 +53,34 @@ app.jinja_env.globals.update(_=translator.gettext)
 
 @app.before_request
 def app_before_request():
+    modifications = watch_files_date(app)
+    # check added / modified
+    for k, v in modifications.iteritems():
+        modified = app.db['modifications'].get(k)
+        if not modified or modified['modified'] != v['modified']:
+            if not modified:
+                print 'added -->', v['_id']
+            else:
+                print 'modified -->', v['_id']
+            file_id = v['_id']
+            file_data = load_single_file(k, v['_id'])
+            if file_data:
+                app.db['files'][file_id] = file_data
+                for key in file_data['keywords']:
+                    app.db['keys'][key] = file_id
+    # check removed
+    for k, v in app.db['modifications'].iteritems():
+        if k not in modifications:
+            print 'removed -->', v['_id']
+            removed = app.db['files'].get(v['_id'])
+            if removed:
+                for key in removed['keywords']:
+                    if app.db['keys'].get(key) == removed['_id']:
+                        del app.db['keys'][key]
+                del app.db['files'][v['_id']]
+
+    app.db['modifications'] = modifications
+
     g.files = app.db['files']
     g.keys = app.db['keys']
 
--- loaders.py
+++ loaders.py
@@ -26,63 +26,56 @@ def load_config(app, config_name='config.py'):
     app.config.setdefault('DEFAULT_MESSAGE', u'...')
 
 
+def watch_files_date(app):
+    print '----------- Watch modification -----------'
+    content_dir = app.config.get('DATA_DIR')
+
+    all_files = _all_file_paths(app)
+    files_modified = {}
+
+    for f in all_files:
+        if _is_invisible(f, content_dir):
+            continue
+        file_id = os.path.splitext(os.path.basename(f))[0]
+        try:
+            f_mtime = os.path.getmtime(f)
+        except OSError:
+            f_mtime = 0
+        files_modified[f] = {
+            '_id': file_id,
+            'modified': f_mtime
+        }
+    return files_modified
+
+
 def load_files(app):
     print '----------- Load Files -----------'
     content_dir = app.config.get('DATA_DIR')
-    content_ext = app.config.get('DATA_FILE_EXT')
 
-    all_files = []
-    for root, directory, files in os.walk(content_dir):
-        file_full_paths = [
-            os.path.join(root, f)
-            for f in filter(lambda x: x.endswith(content_ext), files)
-        ]
-        all_files.extend(file_full_paths)
-
-    file_ids = []
-    file_data = {}
+    all_files = _all_file_paths(app)
+    files_data = {}
     for f in all_files:
-        relative_path = f.split(content_dir + '/', 1)[1]
-        if relative_path.startswith('_'):
+        if _is_invisible(f, content_dir):
             print '***', f
             continue
 
         file_id = os.path.splitext(os.path.basename(f))[0]
         print '-->', f, '=>', file_id
+
         try:
-            with open(f, 'r') as fh:
-                meta_string = fh.read().decode('utf-8')
+            files_data[file_id] = _single_file(f, file_id)
         except Exception as e:
-            e.current_file = f
-            app.logger.error(e)
-            continue
-        try:
-            meta = _parse_file_meta(meta_string)
-        except Exception as e:
-            e.current_file = f
             app.logger.error(e)
             continue
 
-        items = [_prase_news_item(item) for item in meta.pop('messages', [])]
-        _type = meta.pop('type', None)
-        if not _type:
-            _type = 'news' if items else 'text'
+    return files_data
 
-        file_data.update({
-            file_id: Entry({
-                '_id': file_id,
-                'keywords': meta.pop('keywords', meta.pop('keys', [])),
-                'type': _type,
-                'status': meta.pop('status', 1),
-                'text': meta.pop('text', u''),
-                'messages': items[:8],  # max 8 entries
-            }, f)
-        })
-        file_ids.append(file_id)
 
-    print '\n'
-
-    return file_data
+def load_single_file(file_path, file_id):
+    try:
+        return _single_file(file_path, file_id)
+    except Exception:
+        return None
 
 
 def load_keys(app, file_data):
@@ -112,6 +105,57 @@ def load_keys(app, file_data):
 
 
 # helpers
+def _all_file_paths(app):
+    content_dir = app.config.get('DATA_DIR')
+    content_ext = app.config.get('DATA_FILE_EXT')
+
+    all_files = []
+    for root, directory, files in os.walk(content_dir):
+        file_full_paths = [
+            os.path.join(root, f)
+            for f in filter(lambda x: x.endswith(content_ext), files)
+        ]
+        all_files.extend(file_full_paths)
+    return all_files
+
+
+def _is_invisible(f, content_dir):
+    relative_path = f.split(content_dir + '/', 1)[1]
+    file_id = os.path.splitext(os.path.basename(f))[0]
+    if relative_path.startswith('_') or file_id.startswith('_'):
+        return True
+    else:
+        return False
+
+
+def _single_file(file_path, file_id):
+    try:
+        with open(file_path, 'r') as fh:
+            meta_string = fh.read().decode('utf-8')
+    except Exception as e:
+        e.current_file = file_path
+        raise e
+    try:
+        meta = _parse_file_meta(meta_string)
+    except Exception as e:
+        e.current_file = file_path
+        raise e
+
+    items = [_prase_news_item(item) for item in meta.pop('messages', [])]
+    _type = meta.pop('type', None)
+    if not _type:
+        _type = 'news' if items else 'text'
+    entry = Entry({
+        '_id': file_id,
+        'keywords': meta.pop('keywords', meta.pop('keys', [])),
+        'type': _type,
+        'status': meta.pop('status', 1),
+        'text': meta.pop('text', u''),
+        'messages': items[:8],  # max 8 entries
+    }, file_path)
+    return entry
+
+
 def _prase_news_item(item):
         return {
             'title': item.get('title', u''),
